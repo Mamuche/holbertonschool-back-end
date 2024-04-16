2f166455dc1f9d460885124619e3fe0859eab2b6
Mamuche <marionlaroche17@gmail.com>
2024-04-16 14:29:40 +0200

    task3 update loop for user

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -35,17 +35,17 @@ if __name__ == '__main__':
         response_todo = requests.get(f'{api}/todos?userId={user_id}')
         tasks = response_todo.json()
 
-    """Prepare tasks list for this user"""
-    user_tasks = []
-    for task in tasks:
-        user_tasks.append({
-            "username": username,
-            "task": task['title'],
-            "completed": task['completed']
-        })
+        """Prepare tasks list for this user"""
+        user_tasks = []
+        for task in tasks:
+            user_tasks.append({
+                "username": username,
+                "task": task['title'],
+                "completed": task['completed']
+            })
 
-    """Store user tasks in the dictionary with user_id as key"""
-    all_user_tasks[user_id] = user_tasks
+        """Store user tasks in the dictionary with user_id as key"""
+        all_user_tasks[user_id] = user_tasks
 
     """Write all_user_tasks to a JSON file"""
     with open('todo_all_employees.json', 'w') as jsonfile:
